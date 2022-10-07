f1cc1ab4b797d87307b25d02216b9c57c7790bec
Sergio <sherrera725@gmail.com>
2022-10-06 22:13:42 -0700

    Initial Commit

---
+++ PlantStools/PlantStoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantStoolsApp.swift
+//  PlantStools
+//
+//  Created by Sergio Herrera on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PlantStoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
